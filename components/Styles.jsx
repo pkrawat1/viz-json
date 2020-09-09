846c4b9c5de9a10763330f298586aac95d5d5f88
Pankaj Kumar Rawat <pankajrawat19sept@gmail.com>
2020-09-09 23:23:43 +0530

    Added file uploader with some UI refactoring

---
+++ components/Styles.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  height: 20px;
+  width: 20px;
+  padding: 0;
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin-right: 0.3rem;
+`;
--- components/index.js
+++ components/index.js
@@ -1,5 +1,5 @@
 export { default as FileReader } from './FileReader';
 export { default as JsonQueryInput } from './JsonQueryInput';
 export { default as JsonNavigator } from './JsonNavigator';
-export {default as JsonNodeList} from './JsonNodeList';
-export { default as JsonNodeObject} from './JsonNodeObject';
+export { default as JsonNodeList } from './JsonNodeList';
+export { default as JsonNodeObject } from './JsonNodeObject';
